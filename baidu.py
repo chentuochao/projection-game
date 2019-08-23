c31be0f85e243143d5c2d2c70876ca34e451f6fc
bob-lytton <39236531+bob-lytton@users.noreply.github.com>
2019-08-23 15:34:46 +0800

    add baidu api setting

---
+++ baidu.py
@@ -0,0 +1,12 @@
+from aip import AipBodyAnalysis
+import numpy as np
+import time
+import cv2
+
+
+APP_ID = '16990646'
+API_KEY = 'kQKVGNVUzUgAEGkquWWuBYC0'
+SECRET_KEY = '8DQNeOOIWK4BTQ1ZZL8uYaipiXycrApD'
+
+client = AipBodyAnalysis(APP_ID, API_KEY, SECRET_KEY)
+
